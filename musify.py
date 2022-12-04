0c65c30fd52aca059c3205ed80b7d7b71daa22f3
Sai Teja Vadagam <109232045+Sai-Teja-Vadagam@users.noreply.github.com>
2022-12-04 15:01:15 +0530

    Update musify.py

--- musify.py
+++ musify.py
@@ -1,37 +1,68 @@
 from tkinter import *
-from tkinter import filedialog
 from pygame import mixer
+from customtkinter import *
+from PIL import ImageTk, Image
+from tkinter import ttk
+from mutagen.mp3 import MP3
+import time
+
+
 
 mixer.init()
 
 songlist = []
+songnames = []
+background = "#333333"  # light black
 
 
 def add_song():
     song = filedialog.askopenfilenames(initialdir='songslib/', filetypes=(("mp3 files", "*.mp3"),))
     for i in song:
-        songlist.append(str(i).replace("C:/Users/saite/PycharmProjects/musify/", ""))
+        i = str(i).replace("C:/Users/saite/PycharmProjects/musify/", "")
+        if i not in songlist:
+            songlist.append(i)
+    addtobox()
 
 
 def printsong():
     for x in songlist:
-        x = x.replace('songslib/','')
-        x = x.replace('.mp3','')
+        x = x.replace('songslib/', '')
+        x = x.replace('.mp3', '')
         print(x)
     print(len(songlist))
 
 
+# some global variables
 count = 0
+play = False
+j = 0
 
 
 def play_song():
-    global songlist
-    mixer.music.load(str(songlist[count]))
-    mixer.music.play(loops=0)
+    global songlist, play, count, j
+
+    i = displaybox.curselection()
+    if i != j:
+        if len(i) != 0:
+            count = i[0]
+    j = i
+    if len(songlist) == 0:
+        print("Songs List is empty!!...")
+    else:
+        mixer.music.load(str(songlist[count]))
+        mixer.music.play(loops=0)
+        play = True
+        name = "Playing:-\t" + songnames[count] + " " * 200
+        Label(root, text=name, bg=background, fg='white', font=("Arial", 16)).place(x=300, y=450)
+        song_bar.stop()
+        startprogress()
 
 
 def stop_song():
+    global play, song_bar
     mixer.music.stop()
+    play = False
+    song_bar.stop()
 
 
 def next_song():
@@ -41,6 +72,7 @@ def next_song():
         count += 1
     else:
         count = 0
+    song_bar.stop()
     play_song()
 
 
@@ -50,18 +82,35 @@ def prev_song():
         count -= 1
     else:
         count = len(songlist) - 1
+    song_bar.stop()
     play_song()
 
 
+def songlength():
+    global songlist
+    song = MP3(songlist[count])
+    song_info = song.info
+    length = int(song_info.length)
+    return length
+
+
 root = Tk()
-root.title("♫ Player")
-root.geometry('600x300')
-root.configure(bg='grey')
+root.title('Musify')
+root.iconbitmap('icons/biticon.ico')
+root.geometry('1280x800')
+root.resizable(False, True)
+root.configure(bg=background)
+
+wf = root.winfo_width() / 600
+hf = root.winfo_height() / 300
+
+
 # creating a menu bar
 
 
 def displaylistbox():
-    list_box = Listbox(root, bg='black', fg='green', selectbackground='grey', selectforeground='black', width=70)
+    list_box = Listbox(root, bg='black', fg='#00ff00', font=("Courier", 16), selectbackground='blue',
+                       selectforeground='white', width=300, height=15)
     return list_box
 
 
@@ -69,14 +118,17 @@ def addtobox():
     for i in songlist:
         i = i.replace('songslib/', '')
         i = i.replace('.mp3', '')
-        displaybox.insert(END, i)
+        if i not in displaybox.get(0,END):
+            displaybox.insert(END, i)
+            songnames.append(str(i))
 
 
-def selectplay():
-    global count
-    i = displaybox.curselection()
-    count = i[0]
-    play_song()
+def startprogress():
+    global song_bar
+    song_bar['value']=0
+    step = songlength()*10
+    song_bar.start(step)
+    print(step)
 
 my_menu = Menu(root)
 root.config(menu=my_menu)
@@ -86,26 +138,48 @@ print_menu = Menu(my_menu)
 toggle_display = Menu(my_menu)
 
 my_menu.add_cascade(label='File', menu=add_song_menu)
-my_menu.add_cascade(label='Print',menu=print_menu)
+my_menu.add_cascade(label='Print', menu=print_menu)
 add_song_menu.add_cascade(label='select one song', command=add_song)
 print_menu.add_cascade(label='Click to print song names', command=printsong)
 print_menu.add_cascade(label='Display', command=displaylistbox)
 
 # print_btn = Button(root, text='print', command=printsong).place(x=260, y=150)
 
-Button(root, text='prev', command=prev_song).place(x=220, y=220)
+# images
 
-Button(root, text='play', command=play_song).place(x=260, y=220)
+prev_img = CTkImage(Image.open('icons/back.png').resize((20, 20)))
+play_img = CTkImage(Image.open('icons/play.png').resize((20, 20)))
+next_img = CTkImage(Image.open('icons/next.png').resize((20, 20)))
+stop_img = CTkImage(Image.open('icons/pause.png').resize((20, 20)))
+left_img = ImageTk.PhotoImage(Image.open('icons/disc.png').resize((150, 150)))
+right_img = ImageTk.PhotoImage(Image.open('icons/sound.png').resize((150, 150)))
 
-Button(root, text='stop', command=stop_song).place(x=300, y=220)
+# buttons
+prev_btn = CTkButton(root, text='', image=prev_img, command=prev_song, fg_color='green', hover_color='#00ff00')
 
-Button(root, text='next', command=next_song).place(x=340, y=220)
+prev_btn.place(x=400, y=600)
 
-Button(root, text='Play Selected',command=selectplay).place(x=257, y=180)
+play_btn = CTkButton(root, text='', image=play_img, command=play_song, fg_color='green', hover_color='#00ff00')
+
+play_btn.place(x=550, y=600)
+
+CTkButton(root, text='', image=stop_img, command=stop_song, fg_color='green', hover_color='#00ff00').place(x=550, y=640)
+
+CTkButton(root, text='', image=next_img, command=next_song, fg_color='green', hover_color='#00ff00').place(x=700, y=600)
+
+# image Labels
+
+Label(root, image=left_img, bg=background).place(x=100, y=550)
+Label(root, image=right_img, bg=background).place(x=1025, y=550)
+Label(root, text="Musify - MP3 Player", fg='white', bg=background, font=("Muroslant", 19)).pack(padx=1)
+
 displaybox = displaylistbox()
 
-displaybox.pack()
+displaybox.pack(padx=20, pady=3)
 
-Button(root, text='Reload Songs', command=addtobox).place(x=515,y=20)
+# progress bar
+
+song_bar = ttk.Progressbar(root, orient='horizontal', mode='determinate', length=600)
+song_bar.place(x=320, y=555)
 
 root.mainloop()
